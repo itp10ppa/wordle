5ea9273f551ba08e23702b2fb4e1e22533f914a0
Anastasia Sukmanova <nastya.sukmanova@bk.ru>
2025-11-20 13:36:44 +0500

    Margins

--- game_manager.py
+++ game_manager.py
@@ -1,21 +1,3 @@
-def game_mode_selection():
-    print('--- ВЫБОР РЕЖИМА ИГРЫ ---\n')
-    print('1 - Играть с другом')
-    print('2 - Играть с компьютером')
-
-    while True:
-        try:
-            game_mode_choice = int(input('Ваш выбор (введите число): ').strip())
-
-            if game_mode_choice == 1 or game_mode_choice == 2:
-                return game_mode_choice
-            else:
-                raise ValueError
-
-        except ValueError:
-            print('Ошибка: вы ввели некорректное значение! Попробуйте ещё раз.')
-
-
 def validation():
     while True:
         word = input().strip().upper()
@@ -32,25 +14,24 @@ def play_logic(target_word):
     attempts = []
     current_attempt = 0
     is_winner = False
-    print('Попробуйте угадать слово:')
+    print('\nПопробуйте угадать слово:')
 
     while current_attempt < 6 and not is_winner:
         guess = validation()
         attempts.append(guess)
         current_attempt += 1
+        print('\n' * 40)
 
         for attempt in attempts:
             print(attempt)
 
         if guess == target_word.upper():
             is_winner = True
-            print(f"Победа! Вы угадали слово с {current_attempt} попытки.")
+            print(f"\nПобеда! Вы угадали слово с {current_attempt} попытки.")
         else:
-            print('У вас осталось попыток:', 6 - current_attempt, '\n')
+            print('\nУ вас осталось попыток:', 6 - current_attempt, '\n')
 
     if not is_winner:
         print('Вы не угадали. Загаданное слово было', target_word.upper())
 
     return is_winner
-
-
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from game_manager import game_mode_selection, play_logic, validation
+from game_manager import play_logic, validation
 import random
 
+
 def run_game():
-    print('=' * 40)
-    print(' '* 7 + 'Добро пожаловать в WORDLE!')
-    print('=' * 40)
+
     print('\n--- ПРАВИЛА ИГРЫ --- \n'
           '\nЦель: угадать загаданное из 5 букв слово за 6 попыток.'
           '\nВводите слово из 5 букв и получайте цветные подсказки: '
@@ -12,10 +11,21 @@ def run_game():
           '\n ** желтый - буква есть в слове, но на другом месте'
           '\n ** без цвета - буквы нет в слове')
 
+    print('\n--- ВЫБОР РЕЖИМА ИГРЫ ---\n'
+          '\n1 - Играть с компьютером'
+          '\n2 - Играть с другом')
 
-    game_mode = game_mode_selection()
 
-    if game_mode == 2:
+    while True:
+        game_mode_choice = int(input('\nВаш выбор (введите число): ').strip())
+
+        if game_mode_choice == 1 or game_mode_choice == 2:
+            break
+        else:
+            print('Ошибка: вы ввели некорректное значение! Попробуйте ещё раз.')
+
+    if game_mode_choice == 1:
+        print('\n' * 40)
         print('Компьютер загадал слово. Введите русское слово из 5 букв, чтобы угадать. У вас 6 попыток. Удачи!')
         words = ['ангар', 'касса', 'стиль', 'принт']
         target_word = random.choice(words)
@@ -23,19 +33,25 @@ def run_game():
 
         play_logic(target_word)
 
-    else:
+    elif game_mode_choice == 2:
+        print('\n' * 40)
         print('Отвернитесь, пока ваш друг загадывает слово')
         print('Введите загаданное слово:')
         friend_word = validation()
-        print('\n' * 10)
+        print('\n' * 40)
 
         play_logic(friend_word)
+
+print('\n' * 10)
+print('=' * 40)
+print(' '* 7 + 'Добро пожаловать в WORDLE!')
+print('=' * 40)
 
 run_game()
 
 
 while True:
-    a = input('Сыграть еще раз? Да/нет: ').strip().upper()
+    a = input('\n\nСыграть еще раз? Да/нет: ').strip().upper()
 
     if a == 'ДА':
         run_game()
