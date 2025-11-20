61d4a4e280e41391bba74003ae38e3ea614a52af
Anastasia Sukmanova <nastya.sukmanova@bk.ru>
2025-11-20 11:42:43 +0500

    Add play_vs_computer function

--- game_manager.py
+++ game_manager.py
@@ -1,3 +1,6 @@
+import random
+
+
 def game_mode_selection():
     print('--- ВЫБОР РЕЖИМА ИГРЫ ---\n')
     print('1 - Играть с другом')
@@ -18,22 +21,42 @@ def game_mode_selection():
 
 def validation():
     while True:
-        try:
-            guess = input('Введите вашу догадку: ').strip()
+        guess = input('Введите вашу догадку: ').strip()
 
-            if not all('а' <= char <= 'я' for char in guess):
-                print('Слово должно содержать только русские буквы! Попробуйте еще раз.')
-            elif len(guess) != 5:
-                print('Слово должно состоять из 5 букв! Попробуйте еще раз.')
-            else:
-                return guess
+        if not all('а' <= char <= 'я' for char in guess):
+            print('Слово должно содержать только русские буквы! Попробуйте еще раз.')
+        elif len(guess) != 5:
+            print('Слово должно состоять из 5 букв! Попробуйте еще раз.')
+        else:
+            return guess
 
-        except ValueError:
-            print('Ошибка: вы ввели некорректное значение! Попробуйте ещё раз.')
 
 
 def play_vs_computer():
-    print()
+    words = ['ангар', 'касса', 'стиль', 'принт']
+    target_word = random.choice(words)
+    print(target_word)
+
+    attempts = []
+    current_attempt = 0
+    is_winner = False
+
+    print('Компьютер загадал слово. Введите русское слово из 5 букв, чтобы угадать. У вас 6 попыток. Удачи!')
+
+    while current_attempt < 6 and not is_winner:
+        gues = validation()
+        attempts.append(gues)
+        current_attempt += 1
+
+        for attempt in attempts:
+            print(attempt.upper())
+
+        if gues == target_word:
+            is_winner = True
+            print('WIN')
+
+    if not is_winner:
+        print('Вы не угадали. Загаданное слово было', target_word.upper())
 
 
 def play_vs_friend():
